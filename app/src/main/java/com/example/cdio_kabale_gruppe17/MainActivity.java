af32d88a2a7e173a652d5f1e83425a3ff879c208
ColleDK <s195490@student.dtu.dk>
2021-02-16 17:58:27 +0100

    opencv added to the project

--- app/src/main/java/com/example/cdio_kabale_gruppe17/MainActivity.java
+++ app/src/main/java/com/example/cdio_kabale_gruppe17/MainActivity.java
@@ -3,8 +3,18 @@ package com.example.cdio_kabale_gruppe17;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
+    private static String TAG = "opencv";
+    static {
+        if (!OpenCVLoader.initDebug())
+            Log.d(TAG, "Unable to load OpenCV");
+        else
+            Log.d(TAG, "OpenCV loaded");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
